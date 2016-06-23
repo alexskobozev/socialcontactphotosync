bab91966caab5d50818383745c75c83debab2fb7
Alexander Kobozev <eternalkaif@gmail.com>
2016-06-23 22:22:16 +0300

    Start with RX

--- app/src/main/java/com/wishnewjam/socialcontactphotosync/CollectContactsMaster.java
+++ app/src/main/java/com/wishnewjam/socialcontactphotosync/CollectContactsMaster.java
@@ -1,21 +1,95 @@
 package com.wishnewjam.socialcontactphotosync;
 
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.os.Build;
+import android.provider.ContactsContract;
+
+import java.util.ArrayList;
+
 import rx.Observable;
 import rx.Subscriber;
 
 public class CollectContactsMaster
 {
+	private final static String[] FROM_COLUMNS = {
+			Build.VERSION.SDK_INT
+					>= Build.VERSION_CODES.HONEYCOMB ?
+					ContactsContract.Contacts.DISPLAY_NAME_PRIMARY :
+					ContactsContract.Contacts.DISPLAY_NAME
+	};
 
-	Observable<String> collectPhoneContactsObservable = Observable.create(
-			new Observable.OnSubscribe<String>()
+	private static final String[] PROJECTION =
 			{
-				@Override
-				public void call(Subscriber<? super String> subscriber)
+					ContactsContract.Contacts._ID,
+					ContactsContract.Contacts.LOOKUP_KEY,
+					ContactsContract.Contacts.DISPLAY_NAME_PRIMARY
+			};
+
+	private static final String SELECTION =
+
+			ContactsContract.Contacts.DISPLAY_NAME_PRIMARY + " LIKE ?";
+
+	private final static int[] TO_IDS = {
+			android.R.id.text1
+	};
+
+	public void collectContacts()
+	{
+
+	}
+
+	public static Observable<ArrayList<String>> getCollectPhoneContactsObservable(ContentResolver
+			                                                                       contentResolver)
+	{
+		return Observable.create(
+				new Observable.OnSubscribe<ArrayList<String>>()
 				{
-					// collect contact
-					subscriber.onNext("Hi");
-					subscriber.onCompleted();
+					@Override
+					public void call(Subscriber<? super ArrayList<String>> subscriber)
+					{
+						Cursor cur = contentResolver.query(ContactsContract.Contacts.CONTENT_URI,
+								null, null, null, null);
+
+						ArrayList<String> contactNamesArrayList = new ArrayList<>();
+						if (cur != null && cur.getCount() > 0)
+						{
+							while (cur.moveToNext())
+							{
+								String id = cur.getString(
+										cur.getColumnIndex(ContactsContract.Contacts._ID));
+								String name = cur.getString(cur.getColumnIndex(
+										ContactsContract.Contacts.DISPLAY_NAME));
+
+								contactNamesArrayList.add(name);
+
+//								if (cur.getInt(cur.getColumnIndex(
+//										ContactsContract.Contacts.HAS_PHONE_NUMBER)) > 0)
+//								{
+//									Cursor pCur = contentResolver.query(
+//											ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+//											null,
+//											ContactsContract.CommonDataKinds.Phone.CONTACT_ID +
+//													" = ?",
+//											new String[]{id}, null);
+//									while (pCur.moveToNext())
+//									{
+//										String phoneNo = pCur.getString(pCur.getColumnIndex(
+//												ContactsContract.CommonDataKinds.Phone.NUMBER));
+//
+//									}
+//									pCur.close();
+//								}
+							}
+							cur.close();
+						}
+
+						// collect contact
+						subscriber.onNext(contactNamesArrayList);
+
+						subscriber.onCompleted();
+					}
 				}
-			}
-	);
+		);
+	}
 }
--- app/src/main/java/com/wishnewjam/socialcontactphotosync/MainActivity.java
+++ app/src/main/java/com/wishnewjam/socialcontactphotosync/MainActivity.java
@@ -1,15 +1,96 @@
 package com.wishnewjam.socialcontactphotosync;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity
+import com.jakewharton.rxbinding.view.RxView;
+import com.trello.rxlifecycle.components.RxActivity;
+
+public class MainActivity extends RxActivity
 {
+	// TODO:LIST
+	// 1. MVP
+	// 2. VK
+	// 3. NOTIFICATIONS
+	// 4. LIST
+
+	private static final String TAG = "RXTesting";
+	private static final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 24234;
+
+	Button synchroButton;
+	private rx.functions.Action1<java.lang.Throwable> myErrorHandler =
+			throwable -> Log.d(TAG, "Error " + throwable.getMessage());
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		synchroButton = (Button) findViewById(R.id.btn_vk_sync);
+
+		RxView.clicks(synchroButton).subscribe(aVoid -> {
+
+			if (ContextCompat.checkSelfPermission(this,
+					Manifest.permission.READ_CONTACTS)
+					!= PackageManager.PERMISSION_GRANTED)
+			{
+				if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+						Manifest.permission.READ_CONTACTS))
+				{
+					AlertDialog.Builder builder = new AlertDialog.Builder(this);
+					builder.setMessage(R.string.rationale).setCancelable(true).create().show();
+				} else
+				{
+					ActivityCompat.requestPermissions(this,
+							new String[]{Manifest.permission.READ_CONTACTS},
+							MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+				}
+			} else
+			{
+				Toast.makeText(this, "SARY(:", Toast.LENGTH_SHORT).show();
+			}
+		});
+
+//		CollectContactsMaster.getCollectPhoneContactsObservable(getContentResolver()).subscribeOn(
+//				Schedulers.io())
+//				.flatMap()
+	}
+
+	@Override
+	public void onRequestPermissionsResult(int requestCode,
+	                                       String permissions[], int[] grantResults)
+	{
+		switch (requestCode)
+		{
+			case MY_PERMISSIONS_REQUEST_READ_CONTACTS:
+			{
+				// If request is cancelled, the result arrays are empty.
+				if (grantResults.length > 0
+						&& grantResults[0] == PackageManager.PERMISSION_GRANTED)
+				{
+
+					collectContacts();
+				} else
+				{
+					Toast.makeText(this, "SARY(:", Toast.LENGTH_SHORT).show();
+				}
+			}
+		}
+	}
+
+	private void collectContacts()
+	{
+		CollectContactsMaster.getCollectPhoneContactsObservable(getContentResolver())
+				.subscribe(strings -> {
+					strings.forEach(s -> Log.d(TAG, "Contact: " + s));
+				}, myErrorHandler);
 	}
 }
